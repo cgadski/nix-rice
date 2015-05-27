667447e29b1ceae6c106b9d2b58a46f531121453
Magnetic Duck <zenmags@gmail.com>
2015-05-27 15:49:42 +0000

    example file

---
+++ rice.nix
@@ -0,0 +1,29 @@
+{pkgs, config, ...}:
+let
+  stringToFile = contents: (x: x.out) (pkgs.writeTextFile {text = contents; name = "text";});
+  inputs =
+    {
+      vimrc = stringToFile 
+''
+syntax on
+
+set expandtab
+set shiftwidth=2
+set softtabstop=2
+
+set autoindent
+'';
+      bashrc = stringToFile 
+''
+export test=testing
+'';
+    };
+  homeFileOutputs = filepath : map (prefix: prefix + filepath) ["/home/magneticduck" "/home/root"];
+
+in
+    
+(import (/home/magneticduck/git/nix-rice/distribute.nix) {inherit pkgs config;})
+  [
+    {output = homeFileOutputs ".vimrc"; input = inputs.vimrc;}
+    {output = homeFileOutputs ".bashrc"; input = inputs.bashrc;}
+  ]
