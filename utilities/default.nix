2750626c0deb80510e8b48ad9c22e4f8313f8c53
Magnetic Duck <zenmags@gmail.com>
2015-06-08 18:03:25 +0000

    weird bug

--- utilities/default.nix
+++ utilities/default.nix
@@ -2,13 +2,15 @@
 
 let
   makeUtility = file: import file {inherit pkgs;};
+  lib = pkgs.stdenv.lib;
 in
 
 rec {
   distribute = makeUtility ./distribute.nix;
   call = x: x { inherit pkgs config utilities;};
 
-
   # this takes an array of config sets and returns a single config set
-  combineConfigs = configs: throw "how do I do this?";
+  combineConfigs = configs: 
+  # an infinite recursion is encountered here: why?
+    lib.foldl lib.recursiveUpdate {} [];
 }
