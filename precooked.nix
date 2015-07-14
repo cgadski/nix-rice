6dff693ff03922f180588b69e833ac3e2153fe26
Chris Gadzinski <zenmags@gmail.com>
2015-07-14 16:36:52 +0000

    vim should be configured with vimConfigurable, tsktsk

--- precooked.nix
+++ precooked.nix
@@ -3,23 +3,12 @@ nix-rice: with nix-rice;
   simple-i3-and-vim = 
     makeRice (world: with world; 
       let
-        inputs = { vimrc = ./precooked-dots/vimrc; };
-        homeFileOutputs = 
-          filepath : map (prefix: prefix + filepath) 
-            [("/home/" + user + "/") "/root/"]; 
-        font = makeFont {
-          name = "dejavu";
-          size = "12";
-        };
+        font = makeFont { name = "dejavu"; size = "12"; };
       in
       {
-        customFiles = 
-          [ {input = inputs.vimrc; output = homeFileOutputs ".vimrc";}];
-        
         wm = makeWM.i3 {
           inherit font;
           modkey = "Mod4";
-
           term = makeTerm.lilyterm {
             browser = "firefox";
             email = "thunderbird";
